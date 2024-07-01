d0091d3d5c0f01ce2f3c2e3d7d5b0d0b1c767aab
subcreation <in8media@gmail.com>
2024-07-01 11:28:19 -0700

    [Feature] Replaced todo list with navigation to todos view

--- TodoCoreDataExample/ContentView.swift
+++ TodoCoreDataExample/ContentView.swift
@@ -6,83 +6,50 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var selection: String? = "checklist"
+    
     var body: some View {
+        #if os(macOS)
         NavigationView {
             List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+                NavigationLink(destination: TodosView(), tag: "checklist", selection: $selection) {
+                    Label("Todos", systemImage: "checklist")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                NavigationLink(destination: SettingsView(), tag: "gear", selection: $selection) {
+                    Label("Settings", systemImage: "gear")
                 }
             }
-            Text("Select an item")
+            
+            TodosView()
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        .toolbar {
+            ToolbarItem {
+                // Hack to maintain header height
+                Button(action: {}) {
+                    Label("Add Item", systemImage: "plus")
+                }
+                .opacity(0.0)
             }
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        #else
+        TabView {
+            TodosView()
+                .tabItem {
+                    Label("Todos", systemImage: "checklist")
+                }
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
         }
+        #endif
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
 }
